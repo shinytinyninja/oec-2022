db20d5a9a89cb72a1efa5a6af7f2699333ee531c
Bartosh Adam Katarzynski <bkatarzy@uwo.ca>
2022-01-22 14:21:38 -0500

    finished plasticobjects

--- plasticObject.py
+++ plasticObject.py
@@ -2,19 +2,46 @@ from webbrowser import get
 
 
 class plasticObject:
-    def __init__(self, objectID, objectType, cord, plasticAmount, risk):
-        self.id = objectID
-        self.type = objectType
-        self.cord = cord
+    def __init__(self, objectID, objectType, latCord, longCord, plasticAmount, risk):
+        self.objectID = objectID
+        self.objectType = objectType
+        self.latCord = latCord
+        self.longCord = longCord
         self.plasticAmount = plasticAmount
         self.risk = risk
         
     def getID(self):
-        return self.id
+        return self.objectID
     
     def setID(self, id):
-        self.id = id
+        self.objectID = id
     
-    def getType(self, type)
-    
-    def set
+    def getObjectType(self):
+        return self.objectType
+
+    def setObjectType(self, myObjectType):
+        self.objectType = myObjectType
+
+    def getLatCord(self):
+        return self.latCord
+
+    def setLatCord(self, myLatCord):
+        self.latCord = myLatCord
+
+    def getLongCord(self):
+        return self.longCord
+
+    def setLongCord(self, myLongCord):
+        self.longCord = myLongCord
+
+    def getPlasticAmount(self):
+        return self.plasticAmount
+
+    def setPlasticAmount(self, myPlasticAmount):
+        self.plasticAmount = myPlasticAmount
+
+    def getRisk(self):
+        return self.risk
+
+    def setRisk(self, myRisk):
+        self.risk = myRisk
