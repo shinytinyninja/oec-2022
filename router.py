1c10a959e1fac6e0e2df673b867df660eae6e752
shinytinyninja <43597960+shinytinyninja@users.noreply.github.com>
2022-01-22 18:50:11 -0500

    boggie

--- router.py
+++ router.py
@@ -79,7 +79,7 @@ class router:
         if self.mode == "Y":
             print("Reading CSV")
         try:
-            self.inputList = read_from_csv("test_cases/small/{}".format(self.fileName))
+            self.inputList = read_from_csv("test_cases/{}".format(self.fileName))
                   
         except:
             print("CSV Error")
